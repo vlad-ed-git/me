fa7a02be82b89891cd59d3255e99f67bf526c9fe
Vladimir Peter <vlad.edna@gmail.com>
2022-02-19 18:25:18 +0800

    initial site

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./main.js"
+  ],
+  theme: {
+    extend: {
+      colors : {
+        'body' : '#17171F',
+        'theme' :  '#3F3FFF',
+        'nav' : '#404053',
+        'secondary' : '#9191A4',
+        'badge' : '#3F3F51',
+        'input-border' : '#565666',
+        'input' : '#2A2A35'
+      },
+      fontFamily: {
+        'bebas': ['Bebas Neue', 'sans-serif'],
+        'sans': ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
